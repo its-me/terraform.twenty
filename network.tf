5080a8f4e54f04d5ab058bf1538fdc310751b345
Sergey Kanafyev <sergeykanafyev@gmail.com>
2026-08-09 18:47:32 +0800

    Pin network module source to v0.1.0

--- network.tf
+++ network.tf
@@ -24,7 +24,7 @@ resource "google_project_service" "apis" {
 # at the same `name` with create = false to read these resources back instead of
 # creating their own.
 module "network" {
-  source = "git::https://github.com/its-me/terraform.module.network.git"
+  source = "git::https://github.com/its-me/terraform.module.network.git?ref=v0.1.0"
 
   project_id = var.project_id
   region     = var.region
