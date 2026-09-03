dfc53e74bb02160c01027c8862c54f6592192557
Sergey Kanafyev <sergeykanafyev@gmail.com>
2026-09-04 01:05:14 +0800

    Pin terraform.module.postgresql to v0.1.5

--- postgresql.tf
+++ postgresql.tf
@@ -2,7 +2,7 @@
 # created by terraform.infrastructure; this reads the same `name` back (create =
 # false) instead of creating its own, managing its own database/user on top.
 module "postgresql" {
-  source = "git::https://github.com/its-me/terraform.module.postgresql.git?ref=v0.1.4"
+  source = "git::https://github.com/its-me/terraform.module.postgresql.git?ref=v0.1.5"
 
   project_id = var.project_id
   region     = var.region
